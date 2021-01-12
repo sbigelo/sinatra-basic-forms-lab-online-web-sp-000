e33eff87f4e362dbd39f996956e2c0bfce3915a9
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-12 17:56:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,22 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    # pl = Puppy.new
+    @puppy = Puppy.new(params['name'], params['breed'], params['age'])
+    erb :display_puppy
+  end
+
 end
+
+
+# pl = PigLatinizer.new
+#     @piglatin = pl.piglatinize(params[:user_phrase])
