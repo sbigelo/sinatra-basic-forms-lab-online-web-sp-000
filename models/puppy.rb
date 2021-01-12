bd28e6d4e041c53f1683ebc4ff38ea758d0319d8
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-12 17:41:15 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,20 +1,15 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
 class Puppy
-attr_accessor :name, :age
-attr_reader :breed
+attr_accessor :name, :age, :breed
 
-@puppy = []
 
   def initialization(name, breed, age)
     @name = name
     @breed = breed
     @age = age
-    @puppy << self
   end
 
-  def self.all
-    @puppy
-  end
+
 
 
 
