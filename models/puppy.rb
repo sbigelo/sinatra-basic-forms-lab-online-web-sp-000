69aa76ce5fe7205a837152e559889664a39035e5
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-12 17:28:05 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,1 +1,20 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
+class Puppy
+attr_accessor :name, :breed, :age
+
+@@all = []
+
+  def initialization(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
+
+
+end
